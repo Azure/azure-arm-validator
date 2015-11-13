7e31ffea6482eafefb04b47673ce78899efa323f
Steven Edouard <sedouard@microsoft.com>
2015-11-13 14:47:33 -0800

    feat(routes/validate) - automated template deployments

--- routes/validate.js
+++ routes/validate.js
@@ -3,18 +3,25 @@ var express = require('express'),
     azureTools = require('../modules/azure'),
     Guid = require('guid'),
     fs = require('fs'),
+    conf = require('../modules/config');
     RSVP = require('rsvp');
+
 var debug = require('debug')('arm-validator:server');
-router.post('/', function(req, res, next) {
+
+function writeFileHelper (fs, fileName, parametersFileName, template, parameters) {
+  var writeFile = RSVP.denodeify(fs.writeFile);
+  return writeFile.call(fs, fileName, JSON.stringify(template, null, '\t'))
+  .then(function () {
+    return writeFile.call(fs, parametersFileName, JSON.stringify(parameters, null, '\t'));
+  })
+}
+router.post('/validate', function(req, res, next) {
 
   var fileName = Guid.raw(),
-      parametersFileName = Guid.raw(),
-      writeFile = RSVP.denodeify(fs.writeFile);
+      parametersFileName = Guid.raw();
+      
 
-  writeFile.call(fs, fileName, JSON.stringify(req.body.template, null, '\t'))
-  .then(function () {
-    return writeFile.call(fs, parametersFileName, JSON.stringify(req.body.parameters, null, '\t'));
-  })
+  writeFileHelper(fs, fileName, parametersFileName, req.body.template, req.body.parameters)
   .then(function () {
     debug('wrote: ');
     debug(JSON.stringify(req.body.template, null, '\t'));
@@ -31,6 +38,48 @@ router.post('/', function(req, res, next) {
   .finally(function () {
     fs.unlink(fileName);
     fs.unlink(parametersFileName);
+  });
+});
+
+router.post('/deploy', function (req, res, next) {
+  var fileName = Guid.raw(),
+      rgName = conf.get('RESOURCE_GROUP_NAME_PREFIX') + Guid.raw(),
+      parametersFileName = Guid.raw();
+
+  for (var key in req.body.parameters.parameters) {
+    // for unique parameters replace with a guid
+    if (req.body.parameters.parameters[key].value === conf.get('PARAM_REPLACE_INDICATOR')) {
+      req.body.parameters.parameters[key].value = 'citest' + Guid.raw().replace(/-/g,'').substring(0, 16);
+    }
+  }
+
+  writeFileHelper(fs, fileName, parametersFileName, req.body.template, req.body.parameters)
+  .then(function () {
+    debug('deploying template: ');
+    debug(JSON.stringify(req.body.template, null, '\t'));
+    debug('with paremeters: ');
+    debug(JSON.stringify(req.body.parameters, null, '\t'))
+    return azureTools.testTemplate(fileName, parametersFileName, rgName);
+  })
+  .then(function () {
+    return res.send({result: 'Deployment Sucessful'});
+  })
+  .catch(function (err) {
+    debug(err);
+    return res.status(400).send({error: err.toString(), _rgName: rgName, command: 'azure group deployment create --resource-group (your_group_name) --template-file azuredeploy.json --parameters-file azuredeploy.parameters.json'});
+  })
+  .finally(function () {
+    fs.unlink(fileName);
+    fs.unlink(parametersFileName);
+
+    azureTools.deleteGroup(rgName)
+    .then(() => {
+      debug('Sucessfully cleaned up resource group: ' + rgName);
+    })
+    .catch((err) => {
+      console.error('failed to delete resource group: ' + rgName);
+      console.error(err);
+    });
   });
 });
 
