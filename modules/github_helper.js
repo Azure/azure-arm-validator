38a969a2561a0d92a1409c939bbcb9732d77bd1f
Steven Edouard <sedouard@microsoft.com>
2015-12-10 00:21:37 +0800

    feat(modules/github_helper) - add github helper module

---
+++ modules/github_helper.js
@@ -0,0 +1,27 @@
+var unirest = require('unirest');
+var path = require('path');
+var conf = require('./config');
+var RSVP = require('RSVP');
+
+exports.getPullRequestBaseLink = function (prNumber) {
+
+  return new RSVP.Promise((resolve, reject) => {
+    unirest.get('https://' + path.join('api.github.com/repos/', conf.get('GITHUB_REPO'), '/pulls/', prNumber.toString()))
+    .headers({'User-Agent': 'Mozilla/5.0'})
+    .end(response => {
+
+      if (response.error) {
+        return reject(response.error);
+      }
+
+      return resolve(response.body);
+    });
+  })
+  .then(body => {
+    return 'https://' + path.join('raw.githubusercontent.com', body.head.repo.full_name, body.head.ref);
+  })
+  .catch(err => {
+    console.error(err)
+  });
+  
+}
