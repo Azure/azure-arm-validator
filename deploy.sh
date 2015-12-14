1f76cb081a24d51a25846cd86c4b7ee2e37abd98
Travis CI <sedouard@microsoft.com>
2015-12-15 04:08:31 +0800

    fix(deploy) - use right deploy key

--- deploy.sh
+++ deploy.sh
@@ -15,8 +15,8 @@ GIT_TARGET_URL="${GIT_USERNAME}@${AZURE_WA_GIT_TARGET}:${DOKKU_APPNAME}"
 eval "$(ssh-agent -s)"
 ssh-agent -s
 
-chmod 600 ./dokku-deploy.key
-ssh-add ./dokku-deploy.key
+chmod 600 ./dokku-deploy
+ssh-add ./dokku-deploy
 
 echo 'Private keys added. Starting Dokku Deployment'
 git remote add $GIT_USERNAME $GIT_TARGET_URL
