1320d7bc68a071a7db41752ea99de8fd075049ed
Brian Moore <bmoore@microsoft.com>
2017-09-30 13:02:59 -0500

    service changes

--- deployment-template/configure-bot.sh
+++ deployment-template/configure-bot.sh
@@ -1,3 +1,9 @@
+# this scripts expects the following args
+userName="$1" #user name which will be used as the directory to clone the repo - usually the admin
+artifactsLocation="$2"
+artifactsLocationSasToken="$3"
+
+appPath='var/www/azure-arm-validator'
 
 #install azure cli
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
@@ -15,7 +21,6 @@ sudo apt-get install nodejs-legacy
 sudo npm install forever -g
 sudo npm install -g forever-service -g
 
-
 #Import the public key used by the package management system.
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 
@@ -24,6 +29,7 @@ echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" 
 
 #Reload local package database.
 sudo apt-get update
+
 #Install the MongoDB packages.
 sudo apt-get install -y mongodb-org
 
@@ -34,31 +40,37 @@ sudo service mongod start
 sudo systemctl enable mongod.service
 
 #install bits for the app
-git clone https://github.com/Azure/azure-arm-validator
+#git clone https://github.com/Azure/azure-arm-validator "/home/$userName/azure-arm-validator" #should probably pass this in, instead of hardcoding it
+git clone https://github.com/Azure/azure-arm-validator $appPath #should probably pass this in, instead of hardcoding it
 
-### don't think these will work from bash - need some other option
-#Enter MongoDB shell by running command mongo. 
-#mongo
+# copy the service file
+cp "$appPath/azure-arm-validator.service" 'etc/systemd/system'
 
 #Create a database where ARM validator will store its information.
-#use arm-validator
-
 #We need to have at least one document inserted in the db. Use following command to insert document
-#db.armvalidator.insert({"name":"arm-validator-db"})
+mongo 'arm-validator' --eval 'db["arm-validator"].armvalidator.insert({"name":"arm-validator-db"})'
 
-#Database is now Ready. Update following value in your .config.json file
-#  "MONGO_URL": "mongodb://localhost:27017/arm-validator",
+#enable monog service
+sudo systemctl enable mongod.service
 
-#start the app
-#Npm install - this requires .config.json to be copied
-#If Application is successfully installed, move forward or look at the errors, install required dependencies and try again.
-#Now Application is ready to use. We can test it first by running manually before configuring to run in production. 
-#You might want to look at logs during first run to diagnose issues if any. To enable debug logs, display on console, run following command
-#export DEBUG=*
-#Run app manually by running following command
-#npm start
+#try to download the config file if it was staged.
+curl -f -o "$appPath/.config.json" "$artifactsLocation/.config.json$artifactsLocationSasToken"
+rc=$?
+if test "$rc" != "0"; then
+    echo "curl failed with: $rc"
+else #start the app if the config file was staged
+    echo "start the service if the config file was installed"
+    sudo systemctl daemon-reload
+    sudo systemctl start azure-arm-validator
+    
+    #cd "$appPath"
+    #npm install
+    #sudo forever-service install armvalidator –-script ./bin/www
+    #sudo service armvalidator start
+fi
 
-#Application should start running now, if you enabled Debug before executing you should be able to see message like “listening on port 3000, logged in to subscription” etc.
-#If Application failed, look at the error and troubleshoot further. Common reason of failure is syntax error in .config.json file, fix the file and try running again
-#Application would stop running as soon as you press CTRL+C or exit terminal. We will use forever to configure this as service, which can run in background.
-#Sudo forever-service install armvalidator –-script ./bin/www 
+#change ownership on the app, just to make life easier
+sudo chown -hR $userName "$appPath"
+
+
+
